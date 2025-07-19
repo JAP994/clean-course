dd03db1abd5374b1a70d9228ae2c505fd3d0bda2
JAP994 <sucop@MacBook-Pro-de-Jonathan.local>
2025-07-18 19:44:44 -0500

    03-functions.md: nueva funcion con ternario

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -36,4 +36,14 @@
 
     }
 
+    //Coninue
+      const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+       
+        if ( isDead ) return 1500;
+
+        if ( isSeparated ) return 2500;
+        
+        return isRetired ? 3000 :4000;
+    
+    }
 })();
