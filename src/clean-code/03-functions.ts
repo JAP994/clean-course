cb6da3d68178bcb52c60f0abb0f8c46223f294c8
JAP994 <sucop@MacBook-Pro-de-Jonathan.local>
2025-07-18 19:33:31 -0500

    03-functions.md: funciones bien escritas

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,34 +1,37 @@
 (() => {
 
-    // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
-        console.log({ movieId });
-    }
-
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
+    function getMovieById( id: string ) {
         console.log({ id });
     }
 
-    // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
+    function getMoieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    function getActorBioById( ActorId: string ) {
         console.log({ ActorId });
     }
     
     // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
+    interface Movie {
+        cast:        string[];
+        description: string;
+        rating:      number;
+        title:       string;
+    }
+    function createMovie({cast, description, rating, title}:Movie) {
         console.log({ title, description, rating, cast });
     }
 
     // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
+    function createActor( fullName: string, birthdate: Date ): boolean {
         
         // tarea asincrona para verificar nombre
         // ..
         // ..
         if ( fullName === 'fernando' ) return false;
 
-        console.log('Crear actor');
+        console.log('Crear actor'), birthdate;
         return true;        
 
     }
